a26ed197a2ed395cd7fed549632bd72b18fdc221
Nursultan <77805226+SultankaReal@users.noreply.github.com>
2022-03-23 16:22:12 +0300

    Adding outputs with version info and public ip

--- main.tf
+++ main.tf
@@ -133,3 +133,14 @@ resource "yandex_kubernetes_node_group" "my_node_group" {
     }
   }
 }
+
+
+output "version_info" {
+  value = "${yandex_kubernetes_cluster.regional_cluster_resource_name.master.0.version_info}"
+  description = "Information about cluster version"
+}
+
+output "public_ip_of_k8s_cluster_master" {
+  value = "${yandex_kubernetes_cluster.regional_cluster_resource_name.master.0.external_v4_address}"
+  description = "The public ip of k8s cluster (master)"
+}
