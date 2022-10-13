b4bf1daeb62d3de9eb44841da4078eebb33d2d8c
Nursultan Kalniyazov <sultanidze24@gmail.com>
2022-10-13 12:46:16 +0300

    Adding provider section with token, cloud-id, folder-id and zone into provider.tf file

--- provider.tf
+++ provider.tf
@@ -5,3 +5,10 @@ terraform {
     }
   }
 }
+
+provider "yandex" {
+  token     = var.default_token
+  cloud_id  = var.default_cloud_id
+  folder_id = var.default_folder_id
+  zone      = var.default_zone
+}
